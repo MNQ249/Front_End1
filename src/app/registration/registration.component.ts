67fdb68681b407110a44304babe24cb7d8a2a0eb
Mesfer Alqahtany <mesfer.vip@gmail.com>
2019-01-20 15:07:26 +0300

    Validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -19,17 +19,20 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit() {
      this.myReactiveForm = this.formBuilder.group({
-       email:  ``,
-       nin:  ``,
-       userName: `` ,
-       password: ``,
-       city:  ``,
-       gender: ``,
-       firstName: ``,
-       lastName : ``,
-       age : ``,
-       phone: ``,
-       role: ``
+       email: ([``, Validators.compose([Validators.required, Validators.pattern(/[A-Za-z0-9.!]+@[a-z.]+\.[a-z]{3}/),
+         Validators.pattern(/[^\s]+/)])]),
+       nin:  ([``, Validators.compose([Validators.required,
+         Validators.pattern(/[^\s]+/), Validators.pattern(/[1-2]{1}[0-9]{9}/),
+         Validators.minLength(10), Validators.maxLength(10)])]),
+       userName: ([``, Validators.required, Validators.compose([Validators.pattern(/[a-z0-9.!@]{3,25}/), Validators.minLength(5), Validators.maxLength(25)])]),
+       password: ([``, Validators.compose([Validators.required]), Validators.minLength(8), Validators.maxLength(16), Validators.pattern(/[a-zA-Z0-9!@]/),  Validators.pattern(/[^\s]+/)]),
+       city:  ([``, Validators.required]),
+       gender: ([``, Validators.required]),
+       firstName: ([``, Validators.required]),
+       lastName : ([``, Validators.required]),
+       age : ([``, Validators.required]),
+       phone: ([``, Validators.required, Validators.compose([Validators.minLength(9), Validators.maxLength(9), Validators.pattern(/[5]{1}[0-9]{8}/), Validators.pattern(/[^\s]+/)])]),
+       role: ([``, Validators.required])
      });
   }
    onSubmit() {
